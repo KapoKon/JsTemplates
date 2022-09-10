2063df7a5b0bffc19c31fd19e6eb9b7b82ee48d7
Yalachi <85626828+Yalachi@users.noreply.github.com>
2022-09-10 17:14:22 +0300

    Add files via upload

---
+++ SimpleSwitch.js
@@ -0,0 +1,19 @@
+function caseSwitch(val){
+    var answer = "";
+    switch(val){
+        case 1:
+            answer = "alpha";
+            break;
+        case 2:
+            answer = "beta";
+            break;
+        case 3:
+        case 4:
+        case 5:
+            answer = "Dunno";
+        default:
+            answer = "Chicken Nuggets";
+            break;    }
+        return answer;
+}
+console.log(caseSwitch(2));
