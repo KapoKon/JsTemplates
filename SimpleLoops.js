5dda1b617f120c5ebbd89ca8e5c372b601d868d6
Yalachi <85626828+Yalachi@users.noreply.github.com>
2022-09-11 14:44:13 +0300

    Add files via upload

---
+++ SimpleLoops.js
@@ -0,0 +1,13 @@
+var myArray = [];
+    var i=0;
+    while(i<5){
+        myArray.push(i);
+        i++;
+    }
+console.log(myArray);
+var ourArray=[];
+    var y=0;
+    for(var y=0; y<6; y++){
+        ourArray.push(y);
+    }
+console.log(ourArray);
